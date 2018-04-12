76e65a0a95f722bef42764ef92d64986198e672a
Amr Essam <amressam01@hotmail.com>
2018-04-12 09:45:11 +0200

    initial commit

---
+++ main/main.ino
@@ -0,0 +1,11 @@
+int motorleft_forward = 2;
+int motorright_forward = 5; 
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(motorleft_forward, 
+}
