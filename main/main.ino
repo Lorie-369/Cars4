57f61aef129f3f4fcf9edfa2997a40270fa10b9d
Amr Essam <amressam01@hotmail.com>
2018-04-12 10:49:08 +0200

    forward, right, left, end

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,12 @@
+
 int motorLeft = 4;
 int motorRight = 3;
 
 int sensorLeft = 10;
 int sensorMid = 9;
 int sensorRight = 8;
+
+int lastRead[3] = {0, 0, 0};
 
 // White 0
 // Black 1
@@ -13,5 +16,55 @@ void setup() {
 }
 
 void loop() {
-  // digitalWrite(motorLeft, HIGH);
+  int rLeft = digitalRead(sensorLeft);
+  int rRight = digitalRead(sensorRight);
+  int rMid = digitalRead(sensorMid);
+
+  if (rLeft == 1 && rRight == 1 && rMid == 1) {
+    if (lastRead[0] == 0 && lastRead[1] == 1 && lastRead[2] == 0)
+      Stop();
+    else
+      goForward();
+  }
+  else if (rLeft == 0 && rRight == 0 && rMid == 1) {
+    goForward();
+  }
+  else if (rLeft == 0 && rRight == 1 && rMid == 0) {
+    goLeft();
+  }
+  else if (rLeft == 1 && rRight == 0 && rMid == 0) {
+    goRight();
+  }
+  else {
+    delay(1000);
+    Stop();
+  }
+  writeHistory(rLeft, rMid, rRight);
 }
+
+void goForward() {
+  digitalWrite(motorLeft, HIGH);
+  digitalWrite(motorRight, HIGH);
+}
+
+void Stop() {
+  digitalWrite(motorLeft, LOW);
+  digitalWrite(motorRight, LOW);
+}
+
+void goLeft() {
+  digitalWrite(motorLeft, HIGH);
+  digitalWrite(motorRight, LOW);
+}
+
+void goRight() {
+  digitalWrite(motorLeft, LOW);
+  digitalWrite(motorRight, HIGH);
+}
+
+void writeHistory(int left, int mid, int right) {
+  lastRead[0] = left;
+  lastRead[1] = mid;
+  lastRead[2] = right;
+}
+
