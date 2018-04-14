1b336f2454e761140e50cf8a2e06ea499b7c336a
Amr <amressam01@hotmail.com>
2018-04-14 15:04:05 +0200

    last edits, working fine

--- main/main.ino
+++ main/main.ino
@@ -4,12 +4,15 @@ int motorRight  = 3;
 int sensorLeft      = 10;
 int sensorMid       = 9;
 int sensorRight     = 8;
-int sensorLeftMid   = 12;
-int sensorLeftRight = 11;
+int sensorMidLeft   = 12;
+int sensorMidRight  = 11;
 
 int speedHigh = 255;
-int speedMid  = 150;
-int speedLow  = 80;
+int speedMid  = 0;
+int speedLow  = 0;
+
+int onTrack = 1;
+int offTrack = 0;
 
 int lastRead[5] = {0, 0, 0, 0, 0};
 
@@ -21,27 +24,63 @@ void setup() {
 }
 
 void loop() {
-  int rLeft = 
+  int rLeft = digitalRead(sensorLeft);
+  int rMid = digitalRead(sensorMid);
+  int rRight = digitalRead(sensorRight);
+  int rMidLeft = digitalRead(sensorMidLeft);
+  int rMidRight = digitalRead(sensorMidRight);
+
+  if (rMid == onTrack) {
+    goForward();
+  } else if (rMidLeft == onTrack) {
+    goRight();
+  } else if (rLeft == onTrack) {
+    goFullRight();
+  } else if (rMidRight == onTrack) {
+    goLeft();
+  } else if (rRight == onTrack) {
+    goFullLeft();
+  } else {
+    Stop();
+  }
+
+  writeHistory(0, rMid, 0);
 }
 
 void goForward() {
-  digitalWrite(motorLeft, HIGH);
-  digitalWrite(motorRight, HIGH);
+  analogWrite(motorLeft, speedHigh);
+  analogWrite(motorRight, speedHigh);
 }
 
 void Stop() {
-  digitalWrite(motorLeft, LOW);
-  digitalWrite(motorRight, LOW);
+  // lastRead [0...4] not just the mid sensor
+  if (lastRead[1] == onTrack) 
+    delay(1000);
+  else 
+    delay(25);
+    
+  analogWrite(motorLeft, 0);
+  analogWrite(motorRight, 0);
+}
+
+void goFullLeft() {
+  analogWrite(motorLeft, speedHigh);
+  analogWrite(motorRight, speedLow);
+}
+
+void goFullRight() {
+  analogWrite(motorLeft, speedLow);
+  analogWrite(motorRight, speedHigh);
 }
 
 void goLeft() {
-  digitalWrite(motorLeft, HIGH);
-  digitalWrite(motorRight, LOW);
+  analogWrite(motorLeft, speedHigh);
+  analogWrite(motorRight, speedMid);
 }
 
 void goRight() {
-  digitalWrite(motorLeft, LOW);
-  digitalWrite(motorRight, HIGH);
+  analogWrite(motorLeft, speedMid);
+  analogWrite(motorRight, speedHigh);
 }
 
 void writeHistory(int left, int mid, int right) {
