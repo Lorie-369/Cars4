eea920ac669c2d17e636dd8a36a5bcb410c380e8
Amr <amressam01@hotmail.com>
2018-04-25 15:52:44 +0200

    added the comments

--- main/main.ino
+++ main/main.ino
@@ -7,7 +7,7 @@ int rightSensor     = 8;
 int midLeftSensor   = 12;
 int midRightSensor  = 11;
 
-int highSpeed = 120;
+int highSpeed = 200;
 int normalSpeed  = 0;
 int lowSpeed  = 0;
 
@@ -16,19 +16,23 @@ int offTrack = 0;
 
 int history[5] = {0, 0, 0, 0, 0};
 
-// White 0
-// Black 1
-
 void setup() {
   Serial.begin(9600);
 }
 
+// Check for blank spot
+
 bool blank() {
+
+  // Read the sensors' data
+
   int rLeft = digitalRead(leftSensor);
   int rMid = digitalRead(midSensor);
   int rRight = digitalRead(rightSensor);
   int rMidLeft = digitalRead(midLeftSensor);
   int rMidRight = digitalRead(midRightSensor);
+
+  // If they're all off the track, return 1 (true), else, return 0 (false).
 
   if (rLeft == offTrack &&
     rMid == offTrack &&
@@ -40,12 +44,16 @@ bool blank() {
     return 0;
 }
 
+// Check for more than one sensor on track
+
 bool moreThanOne() {
   int rLeft = digitalRead(leftSensor);
   int rMid = digitalRead(midSensor);
   int rRight = digitalRead(rightSensor);
   int rMidLeft = digitalRead(midLeftSensor);
   int rMidRight = digitalRead(midRightSensor);
+
+  // At least one of five, 1/5 = 0.2, return 1 (true) if true, else return 0 (false)
 
   if ((rLeft + rMid + rRight + rMidLeft + rMidRight) / 5 > 0.2)
     return 1;
@@ -55,9 +63,18 @@ bool moreThanOne() {
 
 void loop() {
 
+  // If the mid sensor is on track, keep moving forward till it gets
+  // off the track
+
   if (digitalRead(midSensor) == onTrack) {
     while (digitalRead(midSensor) == onTrack)
       goForward();
+
+    // If the mid left sensor is on track, keep moving forward till the 
+    // mid sensor gets on the track.
+    // 
+    // Break the while loop when blank spot is detected, or more than a
+    // sensor is on the track.
 
   } else if (digitalRead(midLeftSensor) == onTrack) {
     while (digitalRead(midSensor) == offTrack && blank() == 0 && moreThanOne() == 0)
@@ -75,6 +92,10 @@ void loop() {
     while (digitalRead(midRightSensor) == offTrack && blank() == 0 && moreThanOne() == 0)
       goFullLeft();
   }
+
+  // At the end, stop.
+  // This won't affect the process, while the sensors are on track.
+
   Stop();
 }
 
@@ -111,11 +132,10 @@ void goRight() {
   analogWrite(rightMotor, highSpeed);
 }
 
-// TODO:
-// Expand the history capability to track all the sensors.
-void writeHistory(int left, int mid, int right) {
+void writeHistory(int left, int midleft, int mid, int midright, int right) {
   history[0] = left;
-  history[1] = mid;
-  history[2] = right;
+  history[1] = midleft;
+  history[2] = mid;
+  history[3] = midright;
+  history[4] = right;
 }
-
