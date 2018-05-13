ffae3412b2e5c1f3fcaa5a96a661c3ee248298e4
Amr <amressam01@hotmail.com>
2018-05-13 12:44:34 +0200

    stable ver

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,14 @@
 int leftMotor   = 5;
 int rightMotor  = 3;
 
-int leftSensor      = 10;
-int midSensor       = 9;
-int rightSensor     = 8;
-int midLeftSensor   = 12;
-int midRightSensor  = 11;
+int leftSensor      = 17;
+int midSensor       = 16;
+int rightSensor     = 14;
+int midLeftSensor   = 15;
+int midRightSensor  = 14;
 
-int highSpeed = 120;
-int slowSpeed  = highSpeed * .80; // 80% the highSpeed.
+int highSpeed = 200;
+int slowSpeed  = highSpeed * .60; // 60% the highSpeed.
 int lowSpeed  = 0;
 
 int onTrack = 1;
@@ -22,10 +22,14 @@ bool blank() {
   int rLeft = digitalRead(leftSensor);
   int rMid = digitalRead(midSensor);
   int rRight = digitalRead(rightSensor);
+  int rMidLeft = digitalRead(midLeftSensor);
+  int rMidRight = digitalRead(midRightSensor);
 
   if (rLeft   == offTrack &&
     rMid      == offTrack &&
-    rRight    == offTrack)
+    rRight    == offTrack &&
+    rMidLeft  == offTrack &&
+    rMidRight == offTrack)
     return 1;
   else 
     return 0;
@@ -36,12 +40,12 @@ void loop() {
   int rLeft = digitalRead(leftSensor);
   int rMid = digitalRead(midSensor);
   int rRight = digitalRead(rightSensor);
+  int rMidLeft = digitalRead(midLeftSensor);
+  int rMidRight = digitalRead(midRightSensor);
 
-  if ((rLeft + rMid + rRight) >= 2) {
-    Serial.println("Intersection, Going Forward for 200ms delay.");
+  if (rLeft + rMid + rRight + rMidRight + rMidLeft >= 3) {
     goForward();
     delay(200);
-    Serial.println("Intersection, delay end.");
 
     if (blank()) {
       analogWrite(leftMotor, 0);
@@ -49,7 +53,6 @@ void loop() {
       Serial.println("End.");
 
       while (analogRead(A0) > 70);
-
       Serial.println("Continue.");
     }
 
@@ -57,13 +60,13 @@ void loop() {
     Serial.println("Going Forward.");
     goForward();
 
-  } else if (rLeft == onTrack) {
-    goRight();
-    Serial.println("Going Right");
+  } else if (rMidLeft == onTrack || rLeft == onTrack) {
+      goRight();
+      Serial.println("Going Right");
 
-  } else if (rRight == onTrack) {
-    goLeft();
-    Serial.println("Going Left");
+  } else if (rMidRight == onTrack || rRight == onTrack) {
+      goLeft();
+      Serial.println("Going Left");
 
   } else {
     Serial.println("Going Slow");
